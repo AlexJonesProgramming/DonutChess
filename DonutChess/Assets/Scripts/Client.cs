2cfaa4286656056324aa60440ddfa71e0d323581
Alex <kjakarproductions@gmail.com>
2020-01-18 17:02:05 -0500

    Added to portfolio
    Added to portfolio

---
+++ DonutChess/Assets/Scripts/Client.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+public class Client : MonoBehaviour
+{
+
+    public void Start()
+    {
+        ExecuteClient();
+    }
+
+
+    // ExecuteClient() Method 
+    static void ExecuteClient()
+    {
+
+        try
+        {
+            // Establish the remote endpoint  
+            // for the socket. This example  
+            // uses port 11111 on the local  
+            // computer. 
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            // Creation TCP/IP Socket using  
+            // Socket Class Costructor 
+            Socket sender = new Socket(ipAddr.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // Connect Socket to the remote  
+                // endpoint using method Connect() 
+                sender.Connect(localEndPoint);
+
+                // We print EndPoint information  
+                // that we are connected 
+                print("Socket connected to -> {0} "+
+                                sender.RemoteEndPoint.ToString());
+
+                // Creation of messagge that 
+                // we will send to Server 
+                byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+                int byteSent = sender.Send(messageSent);
+
+                // Data buffer 
+                byte[] messageReceived = new byte[1024];
+
+                // We receive the messagge using  
+                // the method Receive(). This  
+                // method returns number of bytes 
+                // received, that we'll use to  
+                // convert them to string 
+                int byteRecv = sender.Receive(messageReceived);
+                print("Message from Server -> {0} " +
+                        Encoding.ASCII.GetString(messageReceived,
+                                                    0, byteRecv));
+
+                // Close Socket using  
+                // the method Close() 
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+
+            // Manage of Socket's Exceptions 
+            catch (ArgumentNullException ane)
+            {
+
+                print("ArgumentNullException : {0} "+ ane.ToString());
+            }
+
+            catch (SocketException se)
+            {
+
+                print("SocketException : {0} "+ se.ToString());
+            }
+
+            catch (Exception e)
+            {
+                print("Unexpected exception : {0} "+ e.ToString());
+            }
+        }
+
+        catch (Exception e)
+        {
+
+            print(e.ToString());
+        }
+    }
+}
---
+++ DonutChess/Assets/Scripts/Placement.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placement : MonoBehaviour
+{
+
+    public GameObject piece;
+    public Transform showcase;
+    public GameObject board;
+
+    public float sensitivity, zoom;
+    float previousX, previousY, boardX, boardY;
+
+    bool started = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            
+            if (!started)
+            {
+                started = true;
+                previousX = Input.mousePosition.x;
+                previousY = Input.mousePosition.y;
+                boardX = board.transform.rotation.eulerAngles.x;
+                boardY = board.transform.rotation.eulerAngles.z;
+            }
+            else
+            {
+                float newX = (Input.mousePosition.x - previousX) * -sensitivity;
+                float newY = (Input.mousePosition.y - previousY) * -sensitivity;
+
+                board.transform.RotateAround(Vector3.up, newX);
+                board.transform.RotateAround(Vector3.left, newY);
+
+                previousX = Input.mousePosition.x;
+                previousY = Input.mousePosition.y;
+            }
+            
+        }
+        else
+        {
+            started = false;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            print("click");
+
+
+            if (piece == null)
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.tag != "Board")
+                    {
+                        piece = hit.transform.gameObject;
+                        piece.transform.parent = null;
+
+                        piece.transform.position = showcase.position;
+                        piece.transform.rotation = Quaternion.Euler(-90, 0, 0);
+                    }
+                }
+            }
+            else
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    piece.transform.position = hit.point;
+
+                    piece.transform.forward = hit.normal;
+                    /*float x = Vector3.Angle(Vector3.right, hit.normal);
+                    float y = Vector3.Angle(Vector3.forward, hit.normal);
+                    float z = Vector3.Angle(Vector3.up, hit.normal);
+
+                    piece.transform.rotation = Quaternion.Euler(x - 90, y, z);
+                    print(x + " " + y + " " + z);
+                    */
+
+                    piece.transform.parent = board.transform;
+                    piece = null;
+                }
+            }
+        }
+
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            print("click");
+
+
+            if (piece == null)
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.tag != "Board")
+                    {
+                        Destroy( hit.transform.gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
